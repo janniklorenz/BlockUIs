bfd7fa433e1fc78aba0697943229bfb80b6b849d
Jannik Lorenz <janniklorenz@me.com>
2014-11-18 12:48:07 +0100

    no message

---
+++ BlockButton.h
@@ -0,0 +1,18 @@
+//
+//  IMSButtonItem.h
+//  IMS
+//
+//  Created by Jannik Lorenz on 02.10.14.
+//  Copyright (c) 2014 Intera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// BlockUIBlocks
+#import "BlockUIBlocks.h"
+
+@interface BlockButtonItem : UIBarButtonItem
+
+@property (copy) BlockUIActionBlock selectionBlock;
+
+@end
